2348d8871d5837437ae3648a30eeeea19f1145ea
daniel-esquivel-disney <dani_esquivelc@hotmail.com>
2023-10-28 15:26:04 -0500

    movie details screen

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_details.dart';
 import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
 
 class MovieMapper {
@@ -16,6 +17,27 @@ class MovieMapper {
         posterPath: movieDB.posterPath != ''
             ? 'https://image.tmdb.org/t/p/w500${movieDB.posterPath}'
             : 'no-poster',
+        releaseDate: movieDB.releaseDate,
+        title: movieDB.title,
+        video: movieDB.video,
+        voteAverage: movieDB.voteAverage,
+        voteCount: movieDB.voteCount,
+      );
+
+  static Movie movieDetailsToEntity(MovieDetails movieDB) => Movie(
+        adult: movieDB.adult,
+        backdropPath: movieDB.backdropPath != ''
+            ? 'https://image.tmdb.org/t/p/w500${movieDB.backdropPath}'
+            : 'https://sd.keepcalms.com/i/keep-calm-poster-not-found.png',
+        genreIds: movieDB.genres.map((e) => e.name).toList(),
+        id: movieDB.id,
+        originalLanguage: movieDB.originalLanguage,
+        originalTitle: movieDB.originalTitle,
+        overview: movieDB.overview,
+        popularity: movieDB.popularity,
+        posterPath: movieDB.posterPath != ''
+            ? 'https://image.tmdb.org/t/p/w500${movieDB.posterPath}'
+            : 'https://sd.keepcalms.com/i/keep-calm-poster-not-found.png',
         releaseDate: movieDB.releaseDate,
         title: movieDB.title,
         video: movieDB.video,
